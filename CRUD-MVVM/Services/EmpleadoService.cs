85b4625be6169ae0c78a7c988b629ee2f694f23a
mifcVeritasJR <167486636+mifcVeritasJR@users.noreply.github.com>
2024-11-22 19:24:34 -0600

    Lost in the way

---
+++ CRUD-MVVM/Services/EmpleadoService.cs
@@ -0,0 +1,31 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_MVVM.Services
+{
+    public class EmpleadoService
+    {
+        private readonly SQLiteConnection SQLiteConnection;
+
+        public EmpleadoService()
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "");
+            //Initiqlize Connection
+            SQLiteConnection = new SQLiteConnection(dbPath);
+            //Create Table
+            SQLiteConnection.CreateTable<Empleado>();
+        }
+
+        public List<Empleado> GetAll()
+        {
+            var res = SQLiteConnection.Table<Empleado>().ToList();
+            return res;
+        }
+
+        public int Insert(Empleado empleado);
+    }
+}
