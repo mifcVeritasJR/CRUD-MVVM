02fb0e5e6bf70caa9baec2845d064079e2d9713d
mifcVeritasJR <167486636+mifcVeritasJR@users.noreply.github.com>
2024-11-22 18:36:45 -0600

    Add project files.

---
+++ CRUD-MVVM/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_MVVM
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CRUD-MVVM/Empleado.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+
+namespace CRUD_MVVM
+{
+    public class Empleado
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Nombre { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Email { get; set; }
+    }
+}
